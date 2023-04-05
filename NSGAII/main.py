3a97b48f3cd93c72c33dbd50f4743a676d64993a
Warren Braggs <u01wb19@abdn.ac.uk>
2023-04-05 10:31:06 +0100

    IGD implemented
    Not optimal results.
    TODO: improve the Pareto

--- NSGAII/main.py
+++ NSGAII/main.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from problems.zdt1 import ZDT1
 from problems.zdt2 import ZDT2
 from problems.zdt3 import ZDT3
@@ -5,12 +7,15 @@ from problems.zdt3 import ZDT3
 from DynamicUtils import DynamicUtils
 from Evolution import Evolution
 
-from pymoo.algorithms.moo.nsga2 import NSGA2
 from pymoo.problems import get_problem
+
 from pymoo.indicators.igd import IGD
-import pygmo as pg   
+from pymoo.indicators.hv import HV
+
+from platypus import NSGAII, NSGAIII, DTLZ2, Hypervolume, experiment, calculate, display
 
 import matplotlib.pyplot as plt
+
 
 
 """ Definition of parameters used for NSGAII
@@ -30,13 +35,13 @@ min = 0
 max = 1
 
 
-zdt2 = ZDT2(n_variables)
-dynamic_evolution = DynamicUtils(zdt2, n_individuals, n_generations, n_variables, min, max)
-nsgaii_zdt2 = Evolution(zdt2, n_individuals, n_generations, n_variables, min, max)
+problem = ZDT1(n_variables)
+dynamic_evolution = DynamicUtils(problem, n_individuals, n_generations, n_variables, min, max)
+nsgaii = Evolution(problem, n_individuals, n_generations, n_variables, min, max)
 
-# zdt3 = ZDT3(n_variables)
-# dynamic_evolution = DynamicUtils(zdt3, n_individuals, n_generations, n_variables, min, max)
-# nsgaii_zdt3 = Evolution(zdt3, n_individuals, n_generations, n_variables, min, max)
+# Optimal Pareto Front for ZDT1
+zdt1 = get_problem("zdt1")
+x, y = zdt1.pareto_front().T
 
 
 # Generation of the population and initialization
@@ -45,19 +50,10 @@ for i in range(n_individuals):
     population = dynamic_evolution.generate_random_solutions()
 
 function = []
-for i in nsgaii_zdt2.evolveNSGAII(population):
+for i in nsgaii.evolveNSGAII(population):
     for j in i:
         function.append(j)
 
-# Optimal Pareto Front for ZDT1
-problem = get_problem("zdt2")
-x, y = problem.pareto_front().T
-
-# Optimal Pareto Front for ZDT2
-# problem = get_problem("zdt2")
-# x, y = problem.pareto_front().T
-
-plt.plot(x, y, 'ro')
 
 # Plotting
 function1 = [i[0] for i in function]
@@ -65,11 +61,18 @@ function2 = [i[1] for i in function]
 plt.xlabel('Function 1', fontsize=15)
 plt.ylabel('Function 2', fontsize=15)
 plt.plot(function1, function2, 'bo')
+plt.plot(x, y, 'ro')
 plt.xlim([0, 1])
 plt.ylim([-1, 10])
-plt.show()
+#plt.show()
 
 # Performance Indicators
+function = np.array(function)
+# igd = IGD(zdt1.pareto_front())
+# print("IGD", igd(function))
 
-ind = IGD(function)
-print("IGD", ind(function1))
+hv = HV(zdt1.pareto_front())
+print("HV", hv(function))
+
+# hyp = Hypervolume(minimum=[0, 0], maximum=[11, 11])
+# print(hyp.calculate(function))
