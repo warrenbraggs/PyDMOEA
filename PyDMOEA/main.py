7fe56933880185ebe243c04b84f9e601a86b03e0
Warren Braggs <u01wb19@abdn.ac.uk>
2023-04-14 14:36:25 +0100

    Added COEA to Main

--- PyDMOEA/main.py
+++ PyDMOEA/main.py
@@ -8,7 +8,7 @@ from problems.zdt6 import ZDT6  # not working
 from problems.bnh import BNH    # need to improve MHV
 from pymoo.problems import get_problem
 
-from Utils import Utils
+from Utils import NSGAUtils, MOEADUtils, COEAUtils
 from Evolution import Evolution
 
 from pymoo.indicators.igd import IGD
@@ -34,18 +34,18 @@ min: int
 max: int
 """
 n_individuals = 100 #100
-n_generations = 200 #200
+n_generations = 1000 #200
 n_variables = 30    #2
 min = 0
 max = 1
 
 
-problem = BNH(n_variables)
-dynamic_evolution = Utils(problem, n_individuals, n_generations, n_variables, min, max)
+problem = ZDT1(n_variables)
+dynamic_evolution = NSGAUtils(problem, n_individuals, n_generations, n_variables, min, max)
 algorithm = Evolution(problem, n_individuals, n_generations, n_variables, min, max)
 
 # Optimal Pareto Front for a Test Problem
-pf = get_problem("bnh")
+pf = get_problem("zdt1")
 x, y = pf.pareto_front().T
 
 
@@ -94,10 +94,10 @@ plt.plot(x, y, 'ro', label='Pareto Front')
 plt.plot(f1_nsgaii, f2_nsgaii, 'bo', label='NSGAII')
 # plt.plot(f1_dnsgaiia, f2_dnsgaiia, 'go', label='DNSGAIIA')
 plt.legend(loc="upper right")
-# plt.xlim([0, 1])
-# plt.ylim([-1, 10])
-plt.xlim([0, 10])
-plt.ylim([20, 50])
+plt.xlim([0, 1])
+plt.ylim([-1, 10])
+# plt.xlim([0, 10])
+# plt.ylim([20, 50])
 plt.show()
 
 
@@ -108,4 +108,73 @@ plt.show()
 
 # mhv = HV(dnsgaiia_function_mean)
 # print("MHV", mhv(pf.pareto_front()))
+
+
+
+
+
+
+# n_generations = 100
+# n_individuals = 100
+# n_variables = 2
+
+
+
+# problem = ZDT1(n_variables)
+# moead = MOEADUtils(problem, n_individuals, n_generations, n_variables, min, max)
+# algorithm = Evolution(problem, n_individuals, n_generations, n_variables, min, max)
+
+
+# pf = get_problem("zdt1")
+# x, y = pf.pareto_front().T
+
+# population = moead.generate_random_solutions(n_individuals)
+
+
+# algorithm.evolveMOEAD(population)
+
+
+
+
+'''
+n_generations = 10
+n_individuals = 100
+n_variables = 2
+
+
+
+problem = ZDT1(n_variables)
+coea = COEAUtils(problem, n_individuals, n_generations, n_variables, min, max)
+algorithm = Evolution(problem, n_individuals, n_generations, n_variables, min, max)
+
+
+pf = get_problem("zdt1")
+x, y = pf.pareto_front().T
+
+population = coea.generate_random_solutions(n_individuals)
+coea_function = algorithm.evolveCOEA(population, 10)
+
+
+
+f1_coea = [i[0] for i in coea_function]
+f2_coea = [i[1] for i in coea_function]
+
+coea_function = np.array(coea_function)
+coea_function_mean = get_mean(coea_function)
+
+migd = IGD(pf.pareto_front())
+print("MIGD", migd(coea_function_mean))
+
+mhv = HV(coea_function_mean)
+print("MHV", mhv(pf.pareto_front())/np.prod(coea_function_mean))
+
+plt.plot(x, y, 'ro', label='Pareto Front')
+plt.plot(f1_coea, f2_coea, 'bo', label='COEA')
+plt.legend(loc="upper right")
+plt.xlim([0, 1])
+plt.ylim([-1, 10])
+plt.show()
+
+'''
+
 
