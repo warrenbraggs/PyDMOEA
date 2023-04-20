8fcbbf6d4ff03b78b706ce27a19a35da62ea1692
Warren Braggs <u01wb19@abdn.ac.uk>
2023-04-20 22:42:17 +0100

    Update main.py

--- PyDMOEA/main.py
+++ PyDMOEA/main.py
@@ -12,12 +12,15 @@ from problems.bnh import BNH    # not working
 from problems.df1 import DF1
 from problems.df2 import DF2
 from problems.df3 import DF3
-from problems.df7 import DF7
+from problems.df5 import DF5
+from problems.df7 import DF7    # not working
 from pymoo.problems import get_problem
 
 
 from pymoo.indicators.igd import IGD
 from pymoo.indicators.hv import HV
+from pymoo.visualization.scatter import Scatter
+
 
 
 def get_mean(list):
@@ -38,22 +41,22 @@ max: int
 """
 n_individuals = 100 
 n_generations = 200 
-n_variables = 2   
+n_variables = 30  
 min = 0
 max = 1
 
 # ZDT1, ZDT2 for STATIC: 30 objectives
 # ZDT4 for STATIC: 10 objectives
-# DF1, DF2, DF3, DF7, DF9 for DYNAMIC: 2 objectives
+# DF1, DF2, DF3, DF7, DF9 for DYNAMIC: 2 objectives (used in pymoo)
 
 
 # Initialisation
-problem = DF7(n_generations)
-algorithm = NSGAUtils(problem, n_individuals, n_generations, n_variables, min, max)
+problem = ZDT1(n_generations)
+algorithm = COEAUtils(problem, n_individuals, n_generations, n_variables, min, max)
 evolution = Evolution(problem, n_individuals, n_generations, n_variables, min, max)
 
 # Optimal Pareto Front for testing a problem
-pf = get_problem("df7")
+pf = get_problem("zdt1")
 x, y = pf.pareto_front().T
 
 
@@ -62,7 +65,10 @@ x, y = pf.pareto_front().T
 population = algorithm.generate_random_solutions(n_individuals)
 
 # Evolution of the algorith given the problem
-function = evolution.evolveDNSGAIIB(population, 10)
+# Number of splits for COEA (10 suggested)
+# Number of changes for Dynamic (30 suggested)
+function = evolution.evolveCOEA(population, 10)
+#function = evolution.evolveNSGAII(population)
 
 
 # Performance Indicators
@@ -85,3 +91,4 @@ plt.legend(loc="upper right")
 plt.xlim([0, 1])
 plt.ylim([-1, 7])
 plt.show()
+
