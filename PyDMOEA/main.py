e9ec1f6ec4af4752d073d6eb798924555129ad0f
Warren Braggs <u01wb19@abdn.ac.uk>
2023-05-12 12:47:39 +0100

    Comments Completed

--- PyDMOEA/main.py
+++ PyDMOEA/main.py
@@ -20,72 +20,91 @@ from pymoo.indicators.hv import HV
 
 
 
+def get_mean(list_values):
+    """ Calculate the mean values for MIGD and MHV (Performance Indicators)
 
+    Parameters
+    ----------
+    list_values : float list
+        list of optimal set of solutions (x,y)
 
-def get_mean(list):
+    Returns
+    -------
+    float list
+        mean values of the optimal set of solutions
+    """
     value = 0
-    for i in list:        
-        value = value + i
-    return value/len(list)
+    for i in list_values:        
+        value = value + i  
+    return value/len(list_values)
+
+
+
 
 """ Definition of parameters used for testing an algorithm based on the problem specification
 
-Parameters:
------------
-n_individuals: int
-n_generations: int
-n_variables: int
-min: int
-max: int
+Parameter settings guideline:
+STATIC EVOLUTION (ZDT1, ZDT2): 30 variables
+STATIC EVOLUTION (ZDT4): 10 variables
+DYNAMIC EVOLUTION (DF1, DF2, DF3, DF5): 2 variables 
+
+Parameters
+----------
+n_individuals : int
+n_generations : int
+n_variables : int
+min : int
+max : int
 """
-
-# STATIC EVOLUTION (ZDT1, ZDT2): 30 variables
-# STATIC EVOLUTION (ZDT4): 10 variables
-# DYNAMIC EVOLUTION (DF1, DF2, DF3, DF5): 2 variables 
-
 n_individuals = 100 
-n_generations = 200 
-n_variables = 2
+n_generations = 200
+n_variables = 30
 min = 0
 max = 1
 
-# Initialisation
-problem = DF5(n_generations)
-algorithm = COEAUtils(problem, n_individuals, n_generations, n_variables, min, max)
+# Initialisation of the problem 
+problem = ZDT1(n_generations)
+# Initialisation of the algorithm (NSGAII, COEA, DNSGAIIA, DNSGAIIB, DCOEAA, DCOEAB)
+algorithm = NSGAUtils(problem, n_individuals, n_generations, n_variables, min, max)
+# Initialisation of the evolution (static/dynamic) 
 evolution = Evolution(problem, n_individuals, n_generations, n_variables, min, max)
 
-# Optimal Pareto Front for testing a problem
-pf = get_problem("DF5")
+
+# Definition of the Pareto Optimal Front by pymoo
+pf = get_problem("zdt1")
 x, y = pf.pareto_front().T
 
 
-# Start the stopwatch for measuring the time
+# Start the stopwatch for measuring the time used by the algorithm considered
 start_time = time.time()
 
 
-# Generation of the population and initialization
+# Generation of the population and initialisation
 population = algorithm.generate_random_solutions(n_individuals)
 
-
-# Evolution of the algorith given the problem
-# COEA: number of splits = 10
-# DNSGAIIA/B: number of changes = 30
-# DCOEAA: number of splits = 10, sc ratio = 1
-# DCOEAB: number of splits = 10
-function = evolution.evolveDCOEAB(population, 10)
-
+"""
+Additional parameter settings guideline:
+COEA: number of splits (n_splits) = 10
+DNSGAIIA/B: number of changes (n_solutions) = 30
+DCOEAA: number of splits (n_splits) = 10, stocastic process ratio (sc ratio) = 1
+DCOEAB: number of splits (n_splits) = 10
+"""
+# Start evolution
+function = evolution.evolveCOEA(population, 10)
 
 # End the stopwatch for measuring the time
 end_time = time.time()
-
+# Time calculation
 time = end_time - start_time
+# Display time taken
 print(time)
 
 
-# Performance Indicators
+# Convert the list into numpy array
 function = np.array(function)
 function_mean_value = get_mean(function)
 
+# Performance Indicators
 migd = IGD(pf.pareto_front())
 print("MIGD", migd(function_mean_value)/function_mean_value[1])
 mhv = HV(function_mean_value)
@@ -95,8 +114,10 @@ print("MHV", mhv(pf.pareto_front())/np.prod(function_mean_value))
 # Plotting
 f1 = [i[0] for i in function]
 f2 = [i[1] for i in function]
+# Plot the Pareto Optimal Front
 plt.plot(x, y, 'ro', label='Pareto Front')
-plt.plot(f1, f2, 'bo', label='DCOEAB')
+# Plot the algorithm desired
+plt.plot(f1, f2, 'bo', label='Algorithm')
 plt.legend(loc="upper right")
 plt.xlim([0, 1])
 plt.ylim([-1, 7])
