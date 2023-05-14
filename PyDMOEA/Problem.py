b8bdc8a13fc51db5def6d92f4c02f4192f563d1c
Warren Braggs <u01wb19@abdn.ac.uk>
2023-05-14 18:42:12 +0100

    Comments done

--- PyDMOEA/Problem.py
+++ PyDMOEA/Problem.py
@@ -1,8 +1,14 @@
 from abc import abstractmethod, ABC
 
 class Problem:
-    """TODO: add documentation """
     def __init__(self, n_variables):
+        """ Problem is an abstract class that defines the Multi-Objective Problem (MOP) and Dynamic Multi-Objective Problem (DMOP) used
+
+        Parameters
+        ----------
+        n_variables : int
+            Number of variables
+        """
         self.n_variables = n_variables
         self.min = min
         self.max = max
@@ -10,16 +16,34 @@ class Problem:
     
     @abstractmethod
     def evaluate_objective_values(self, elements):
-        """TODO: add documentation """
+        """ Evaluate the objective values of an individual
+
+        Parameters
+        ----------
+        elements : list
+            List of variables of an individual
+        """
         pass
 
 
     @abstractmethod
     def f1(self, x):
-        """TODO: add documentation """
+        """ Function 1 determined by the specifications of the problem
+
+        Parameters
+        ----------
+        x : list
+            List of variables
+        """
         pass
 
     @abstractmethod
     def f2(self, x):
-        """TODO: add documentation """
+        """ Function 2 determined by the specifications of the problem
+
+        Parameters
+        ----------
+        x : list
+            List of variables
+        """
         pass
